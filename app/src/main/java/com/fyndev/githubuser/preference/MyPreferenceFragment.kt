e0c4a4e42a0ef46365f6135fd2820f4e8acfd5bc
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2021-03-21 11:01:34 +0700

    Move Localization To Preference Setting

--- app/src/main/java/com/fyndev/githubuser/preference/MyPreferenceFragment.kt
+++ app/src/main/java/com/fyndev/githubuser/preference/MyPreferenceFragment.kt
@@ -1,11 +1,24 @@
 package com.fyndev.githubuser.preference
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.fyndev.githubuser.R
 
 class MyPreferenceFragment : PreferenceFragmentCompat() {
+
+    private lateinit var lang: Preference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
+
+        lang = findPreference(resources.getString(R.string.language))!!
+
+        lang.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+            false
+        }
     }
 }
