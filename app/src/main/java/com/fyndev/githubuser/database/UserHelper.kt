ce1323812201120c5b62a08771147c40b880e805
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2021-03-31 11:09:40 +0700

    Delete CLose Function Database

--- app/src/main/java/com/fyndev/githubuser/database/UserHelper.kt
+++ app/src/main/java/com/fyndev/githubuser/database/UserHelper.kt
@@ -15,9 +15,9 @@ class UserHelper(context: Context) {
         private var INSTANCE: UserHelper? = null
 
         fun getInstance(context: Context): UserHelper =
-                INSTANCE ?: synchronized(this) {
-                    INSTANCE ?: UserHelper(context)
-                }
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: UserHelper(context)
+            }
 
         private lateinit var database: SQLiteDatabase
     }
@@ -31,28 +31,20 @@ class UserHelper(context: Context) {
         database = databaseHelper.writableDatabase
     }
 
-    fun close() {
-        databaseHelper.close()
-
-        if (database.isOpen) {
-            database.close()
-        }
-    }
-
     fun getAllUser(): Cursor {
         return database.query(DATABASE_TABLE, null, null, null, null, null, "$_ID ASC")
     }
 
     fun queryById(id: String): Cursor {
         return database.query(
-                DATABASE_TABLE,
-                null,
-                "$_ID = ?",
-                arrayOf(id),
-                null,
-                null,
-                null,
-                null
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null,
+            null
         )
     }
 
